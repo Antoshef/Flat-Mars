d358ffdd331df988c469522c5fe525e7b1b23232
Anton Stanev <antoshef21@gmail.com>
2022-10-23 14:36:43 +0300

    delete output folder

--- out/models.js
+++
@@ -1,7 +0,0 @@
-export var CommandsEnum;
-(function (CommandsEnum) {
-    CommandsEnum["FORWARD"] = "F";
-    CommandsEnum["LEFT"] = "L";
-    CommandsEnum["RIGHT"] = "R";
-})(CommandsEnum || (CommandsEnum = {}));
-//# sourceMappingURL=models.js.map
